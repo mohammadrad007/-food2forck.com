d961ab5428bd5bcf096834b67504e538d9731e20
Mohammad Mohammadi <saberzx313@gmail.com>
2019-04-26 22:25:26 +0430

    Get data From API FOOD2FORK and Make Recipes CMP for show them

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "./component/Form";
 
+const API_KEY = "2cd3fb12cb6b37da9fe2fef820c5bef3";
 class App extends Component {
-  getRecipe = e => {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log("working");
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   };
   render() {
     return (
@@ -14,6 +25,12 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map(recipe => (
+          <div key={recipe.recipe_id}>
+            <img src={recipe.image_url} alt={recipe.title} />
+            <p>{recipe.title}</p>
+          </div>
+        ))}
       </div>
     );
   }
