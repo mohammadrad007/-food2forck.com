960cc61dea5f938294a1096060ba588306146ad9
Mohammad Mohammadi <saberzx313@gmail.com>
2019-04-27 16:33:45 +0430

    make Header

---
+++ src/component/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+  <header className="App-header">
+    <h1 className="App-title">Recipe Search</h1>
+    <h3 className="App-title">Welcome To My WebSite</h3>
+  </header>
+);
+
+export default Header;
