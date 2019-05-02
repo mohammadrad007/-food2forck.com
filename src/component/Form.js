4de9d2df6ffe0667ff3e3d6b9813020bf113e33a
Mohammad Mohammadi <saberzx313@gmail.com>
2019-05-02 19:39:09 +0430

    some another update

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,15 +2,28 @@ import React from "react";
 
 const Form = props => {
   return (
-    <form onSubmit={props.getRecipe} style={{ marginBottom: "2rem" }}>
-      <input
-        className="form__input"
-        type="text"
-        name="recipeName"
-        placeholder="Search Somthings..."
-      />
-      <button className="form__button">Search</button>
-    </form>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+          <h1 className="text-slanted text-capitaliz">
+            search for recipe with{" "}
+            <strong className="text-danger">Food2Fork</strong>
+          </h1>
+          <form onSubmit={props.getRecipe} style={{ margin: "2rem" }}>
+            <input
+              className="form__input"
+              type="text"
+              name="recipeName"
+              placeholder="Search Somthings..."
+            />
+            <button className="form__button">
+              <i className="fa fa-search mr-2" />
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
