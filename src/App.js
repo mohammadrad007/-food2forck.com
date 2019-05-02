7541ebd5a6343d115aff1678191de498ce204e46
Mohammad Mohammadi <saberzx313@gmail.com>
2019-05-02 16:43:19 +0430

    try and catch

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ class App extends Component {
   state = {
     recipes: []
   };
+
   getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`
-    );
+    try {
+      const api_call = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`
+      );
 
-    const data = await api_call.json();
-    this.setState({ recipes: data.recipes });
-    console.log(this.state.recipes);
+      const data = await api_call.json();
+      this.setState({ recipes: data.recipes });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   componentDidMount = () => {
